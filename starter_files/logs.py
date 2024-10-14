b52500dfabd410d16157faadc5ed7c9b09feebbf
Nguyen Hoang <91080631+hknguyen20@users.noreply.github.com>
2024-10-14 13:59:44 +0700

    Update logs.py

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -2,8 +2,12 @@ from azureml.core import Workspace
 from azureml.core.webservice import Webservice
 
 # Requires the config to be downloaded first to the current working directory
-ws = Workspace.from_config()
+session_id = "268650"
+subscription_id= "cdbe0b43-92a0-4715-838a-f2648cc7ad21"
+resource_group= f"aml-quickstarts-{session_id}"
+workspace_name= f"quick-starts-ws-{session_id}"
 
+ws = Workspace.get(name=workspace_name, subscription_id=subscription_id, resource_group=resource_group)
 # Set with the deployment name
 name = ""
 
