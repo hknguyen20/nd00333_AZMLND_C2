c5d3dd02da0d854ea06ebdadb512d3e3cf62941f
Nguyen Hoang <91080631+hknguyen20@users.noreply.github.com>
2024-10-14 07:00:40 +0000

    Update logs.py

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -3,7 +3,7 @@ from azureml.core.webservice import Webservice
 
 # Requires the config to be downloaded first to the current working directory
 session_id = "268650"
-subscription_id= "cdbe0b43-92a0-4715-838a-f2648cc7ad21"
+subscription_id= "aa7cf8e8-d23f-4bce-a7b9-1f0b4e0ac8ee"
 resource_group= f"aml-quickstarts-{session_id}"
 workspace_name= f"quick-starts-ws-{session_id}"
 
